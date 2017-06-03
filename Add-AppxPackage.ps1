ad326f3f6b85e66bce2be17bd35964d6c4033da8
Nguyễn Tuấn <nguyenanhtuan2401@gmail.com>
2017-06-03 14:07:21 +0700

    Create Add-AppxPackage.ps1

---
+++ Add-AppxPackage.ps1
@@ -0,0 +1,80 @@
+# Add-AppxPackage for Current User
+# Created by Nguyen Tuan
+# Website:  www.blogthuthuatwin10.com
+# Github:   github.com/blogthuthuatwin10
+# Facebook: facebook.com/blogthuthuatwin10
+# Twitter:  twitter.com/thuthuatwin10
+# Google+:  plus.google.com/+blogthuthuatwin10
+# Youtube:  youtube.com/blogthuthuatwin10
+
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $args" -Verb RunAs
+	Exit
+}
+
+Function Main-menu()
+{
+    $index=1
+	$apps=Get-AppxPackage -PackageTypeFilter Bundle -AllUsers
+	#return entire listing of applications 
+	    Write-Host "ID`t App name"
+        Write-Host ""
+	foreach ($app in $apps)
+	{
+		Write-Host " $index`t $($app.name)"
+		$index++
+	}
+    if ($apps)
+    {
+		$index++
+        Write-Host ""
+	}
+    else
+    {
+        Write-Host "Apps not found"
+        Write-Host ""
+        pause
+    }  
+    Do
+    {
+        Write-Host ""
+        $IDs=Read-Host -Prompt "For add each app please select ID and press enter"
+    }
+    While($IDs -eq "")
+    
+	#check whether input values are correct
+	try
+	{	
+		[int[]]$IDs=$IDs -split ","
+	}
+	catch
+	{ 
+		 Write-Host "Error:" $_.Exception.Message
+	}
+
+	foreach ($ID in $IDs)
+	{
+		#check id is in the range
+		if ($ID -ge 1 -and $ID -le $apps.count)
+		{
+			$ID--
+			#Remove each app
+			$AppName=$apps[$ID].name
+
+			Get-AppxPackage -AllUsers $AppName | Foreach {Add-AppxPackage -Register "$($_.InstallLocation)\appxmanifest.xml" -DisableDevelopmentMode}
+            pause
+            cls
+            Main-menu
+		}
+		else
+		{
+			Write-Host ""
+            Write-warning -Message "wrong ID"
+            Write-Host ""
+            pause
+            cls
+            Main-menu
+		}
+	}
+}
+Main-menu
