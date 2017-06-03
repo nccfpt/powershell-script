c83d94ca618f26a61fe0cf98847048efbc99ef61
Nguyễn Anh Tuấn <nguyenanhtuan2401@gmail.com>
2017-06-03 10:41:56 +0700

    Update Stop-Service.ps1

--- Stop-Service.ps1
+++ Stop-Service.ps1
@@ -7,7 +7,6 @@
 # Google+:  plus.google.com/+blogthuthuatwin10
 # Youtube:  youtube.com/blogthuthuatwin10
 
-
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
 	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $args" -Verb RunAs
 	Exit
