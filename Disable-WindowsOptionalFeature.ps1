f416d034cdcad8f63e2cce94e9c66d51b9392ea1
Nguyễn Tuấn <nguyenanhtuan2401@gmail.com>
2017-06-03 11:11:44 +0700

    Update Disable-WindowsOptionalFeature.ps1

--- Disable-WindowsOptionalFeature.ps1
+++ Disable-WindowsOptionalFeature.ps1
@@ -17,20 +17,20 @@ Function Main-menu()
     $index=1
 	$Features=Get-WindowsOptionalFeature -Online | ? State -eq 'enabled' | Select FeatureName
 	#return entire listing of applications 
-	    Write-Host "ID`t Feature Name"
-    	Write-Host ""
+	    	Write-Host "ID`t Feature Name"
+    		Write-Host ""
 	foreach ($Feature in $Features)
 	{
 		Write-Host " $index`t $($Feature.FeatureName)"
 		$index++
 	}
     
-    Do
-    {
-        Write-Host ""
-        $IDs=Read-Host -Prompt "For disable feature please select ID and press enter"
-    }
-    While($IDs -eq "")
+    	Do
+    	{
+        	Write-Host ""
+        	$IDs=Read-Host -Prompt "For disable feature please select ID and press enter"
+    	}
+   	While($IDs -eq "")
     
 	#check whether input values are correct
 	try
@@ -51,19 +51,19 @@ Function Main-menu()
 			#Disable each feature
 			$FeatureName=$Features[$ID].FeatureName
 
-            Disable-WindowsOptionalFeature -Online -FeatureName $FeatureName -NoRestart
-            pause
-            cls
-            Main-menu  
+            		Disable-WindowsOptionalFeature -Online -FeatureName $FeatureName -NoRestart
+            		pause
+            		cls
+			Main-menu  
 		}
 		else
 		{
-            Write-Host ""
-            Write-warning -Message "wrong ID"
-            Write-Host ""
-            pause
-            cls
-            Main-menu
+            		Write-Host ""
+            		Write-warning -Message "wrong ID"
+            		Write-Host ""
+            		pause
+            		cls
+            		Main-menu
 		}
 	}
 }
