07910bba0206c9f1b856afdda77ee709f8383e79
Nguyễn Tuấn <nguyenanhtuan2401@gmail.com>
2017-06-04 10:00:32 +0700

    Update Start-Service.ps1

--- Start-Service.ps1
+++ Start-Service.ps1
@@ -1,11 +1,6 @@
 # Start-Service
 # Created by Nguyen Tuan
-# Website:  www.blogthuthuatwin10.com
-# Github:   github.com/blogthuthuatwin10
-# Facebook: facebook.com/blogthuthuatwin10
-# Twitter:  twitter.com/thuthuatwin10
-# Google+:  plus.google.com/+blogthuthuatwin10
-# Youtube:  youtube.com/blogthuthuatwin10
+# Website: www.blogthuthuatwin10.com
 
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
 	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $args" -Verb RunAs
